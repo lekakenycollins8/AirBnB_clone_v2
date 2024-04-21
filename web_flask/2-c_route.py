61cfb4039ad626fcb48382ff44fa33722f9156f5
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-04-21 07:48:16 -0700

    handled variable in route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,5 +19,11 @@ def display_hbnb():
     return "HBNB"
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """displays C followed by value of text"""
+    return "C {}".format(text)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
