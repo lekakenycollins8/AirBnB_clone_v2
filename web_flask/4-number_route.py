4f81cce9fe044f7144e5fde8d5945082567f626a
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-04-21 08:23:54 -0700

    displays n is a number if n is an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Starts a web application and adds another route"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """displays C followed by value of text"""
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    """displays Python followed by text, replaces _ with space"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_int(n):
+    """displays n is a number only if n is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
